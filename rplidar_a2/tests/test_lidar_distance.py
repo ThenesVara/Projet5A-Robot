914fa4522b6f2c8f87a13f25776432ad81076258
ThenesVara <114569016+ThenesVara@users.noreply.github.com>
2022-10-08 22:02:18 +0200

    Update test_lidar_distance.py

--- rplidar_a2/tests/test_lidar_distance.py
+++ rplidar_a2/tests/test_lidar_distance.py
@@ -4,25 +4,6 @@ import subprocess
 
 # Le lidar détecte les points sur une distance seuil défini
 
-'''
-Install rplidar library:
-    pip install rplidar
-    pip3 install rplidar
-
-rplidar : https://github.com/SkoltechRobotics/rplidar?fbclid=IwAR10q_G_yQrJTbZEZB7TMKndGjN466Z-_wv778k1gYo1jV-U1cmCTE5QwGM
-
-Pour RPLIDAR A2M6 and A2M8
-change amount max buffer et baudrate 
--> go to rplidar code -> max_buf_meas value
-                      -> baudrate = 115200
-Pour rplidar A2M7 et A3: 
-                      -> max_buf_meas value
-                      -> baudrate = 256000
-                
-Autorisation usb du lidar:
-Dans le terminal ->  sudo chmod 666 /dev/ttyUSB0
-
-'''
 PORT_NAME = '/dev/ttyUSB0'
 
 #Autorisation usb lidar
